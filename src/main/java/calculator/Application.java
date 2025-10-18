cf771f3ed3f07eb4da893abaa085e2549b2aea0e
bacon <baycun22@naver.com>
2025-10-19 03:19:56 +0900

    feat(validInput): input validation and throw IllegalArgumentException

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,9 +2,42 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Pattern;
+import static java.lang.String.valueOf;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String numberString = Console.readLine();
+        boolean isSimplePattern;
+        String delim = ",|:";
+
+        try {
+            isSimplePattern = validInput(numberString);
+        } catch (IllegalArgumentException e) {
+            return;
+        }
+
+        if (!isSimplePattern) {
+            delim = delim + "|" + Pattern.quote(valueOf(numberString.charAt(2)));
+            try {
+                validDelim(numberString, delim);
+            } catch (IllegalArgumentException e) {
+                return;
+            }
+        }
+    }
+
+    private static boolean validInput(String input) {
+        String simplePattern = "[0-9]+([:,][0-9]+)*";
+        String customPattern = "//[^0-9]\\\\n[0-9]+([^0-9][0-9]+)*";
+        if (input.matches(simplePattern)) return true;
+        if (input.matches(customPattern)) return false;
+        throw new IllegalArgumentException();
+    }
+
+    private static void validDelim(String input, String delim) {
+        String patter = "//[^0-9]\\\\n[0-9]+((" + delim + ")[0-9]+)*";
+        if (!input.matches(patter)) { throw new IllegalArgumentException(); }
     }
 }
